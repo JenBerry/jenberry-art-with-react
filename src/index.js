cd63382b70794c379a8145731ded30f17d00f5b3
Jen Berry <jenberrymail@gmail.com>
2016-02-17 13:05:20 +0000

    hello world

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 var ReactDOM = require('react-dom');
 var React = require('react');
+
+var HelloWorld = React.createClass({
+	render: function(){
+		return(<h1>  hello world </h1>);
+	}
+});
+
+ReactDOM.render(<HelloWorld />, document.body);
