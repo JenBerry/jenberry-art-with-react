e9dcf3ee3b76528e9d70aa55fab480dd00e57acf
Jen Berry <jenberrymail@gmail.com>
2016-03-01 12:23:35 +0000

    add react-router

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,12 @@
 var ReactDOM = require('react-dom');
 var React = require('react');
-var html = require('./index.html')
+
+var Router = require('react-router').Router;
+var Route = require('react-router').Route;
+var Link = require('react-router').Link;
+var hashHistory = require('react-router').hashHistory;
+
+var html = require('./index.html');
 // Uncomment to use Bootstrap javascript elements
 	// global.jQuery = require('jquery');
 	// var bootstrap = require('bootstrap-webpack');
@@ -19,6 +25,7 @@ var Page = React.createClass({
 		<div className="container">
 			<Header page={this.state.page} setPage={this.setPage} />
 			<PageContents page={this.state.page} />
+			<Link to="foo">foo</Link>
 			<Footer />
 		</div>
 		);
@@ -29,5 +36,20 @@ var Header = require('./header.jsx');
 var Footer = require('./footer.jsx');
 var PageContents = require('./page-contents.jsx');
 
+var NotFound = React.createClass({
+	render: function(){
+		return(
+			<h1>Page Not found <Link to="/">Go home</Link></h1>
+		)
+	}
+});
 
-ReactDOM.render(<Page />, document.getElementById('app'));
+
+
+ReactDOM.render((
+	<Router history={hashHistory}>
+		<Route path="/" component={Page} />
+		<Route path="*" component={NotFound} />
+	</Router>
+),
+document.getElementById('app'));
