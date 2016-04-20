19471d0e730441956c1528834e05a87115c4a601
Jen Berry <jenberrymail@gmail.com>
2016-04-20 20:53:42 +0100

    update footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,9 @@ const React = require('react');
 const Footer = React.createClass({
 	render(){
 		return(
-			<footer><hr />Footer</footer>
+			<footer><hr />
+				<p>Website developed by me (Jen Berry) using <a href="https://getbootstrap.com/" target="_blank">Bootstrap</a>, <a href="https://facebook.github.io/react/" target="_blank">React.js</a>, <a href="https://github.com/reactjs/redux" target="_blank">Redux.js</a>, <a href="https://webpack.github.io/" target="_blank">Webpack.js</a>, and <a href="http://expressjs.com/" targer="_blank">Express.js</a>. It is hosted on <a href="https://www.heroku.com/" target="_blank">Heroku</a>. Check out the code on <a href="http://github.com/jenberry" target="_blank">my github account</a></p>
+			</footer>
 		);
 	}
 });
